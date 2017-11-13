4bc9d33044ee6eb146810cbaf867953a68569f87
EPAlmighty <33227475+EPAlmighty@users.noreply.github.com>
2017-11-13 11:37:24 -0500

    Add files via upload
    Tutorial for Module 4

---
+++ M4T1_SalesPrediction_Pinnell.py
@@ -0,0 +1,13 @@
+#CTI-110
+#M4T1
+#Evan Pinnell
+#11/13/17
+
+
+totalSales = float(input('Enter Projected Sales: '))
+
+annualProfit = 0.23
+
+profit = totalSales * annualProfit
+
+print('profit is ', profit)
