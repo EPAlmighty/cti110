95221705e65c2b8af0ba0361fd3c0593b60a0020
EPAlmighty <33227475+EPAlmighty@users.noreply.github.com>
2017-11-19 20:44:15 -0500

    Add files via upload
    HW for Moduel 4

---
+++ M4HW1_DistanceTraveled_Pinnell.py
@@ -0,0 +1,26 @@
+#CTI 110
+#M4HW1_Distance Traveled
+#Evan Pinnell
+#11/19/17
+
+speed = 70
+
+time = 6
+
+timeTwo = 10
+
+timeThree = 15
+
+distanceAfter6 = float(speed * time)
+
+distanceAfter10 = float(speed * timeTwo)
+
+distanceAfter15 = float(speed * timeThree)
+
+
+print ('A car is traveling at 70 miles per hour. Display the following:')
+
+print( 'Distance in 6 hours:', float(distanceAfter6))
+print( 'Distance in 10 hours:', float(distanceAfter10))
+print( 'Distance in 15 hours: ', float(distanceAfter15))
+
